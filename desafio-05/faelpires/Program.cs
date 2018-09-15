e8f484d293604d02ba45c8a44daa567f8b4ce44d
Rafael Pires de Freitas <faelpires@gmail.com>
2018-09-15 00:29:14 -0300

    Performance improvement

--- desafio-05/faelpires/Program.cs
+++ desafio-05/faelpires/Program.cs
@@ -18,6 +18,7 @@ namespace Desafio5
         private static double _totalSalarios;
         private static Funcionario _currentFuncionario;
         private static StringBuilder _stringBuilderHashCode = new StringBuilder(27);
+        private static StringBuilder _stringBuilderResult = new StringBuilder();
         private static readonly Dictionary<int, AreaStats> AreaStatsDict = new Dictionary<int, AreaStats>();
         private static readonly Dictionary<string, SobrenomeStats> SobrenomeStatsDict = new Dictionary<string, SobrenomeStats>();
         private static readonly Dictionary<int, byte[]> AreasDict = new Dictionary<int, byte[]>();
@@ -33,21 +34,23 @@ namespace Desafio5
 
             LoadAndProcessJson(args[0]);
 
-            PrintQuestao1();
-            PrintQuestao2();
-            PrintQuestao3();
-            PrintQuestao4();
+                RunQuestao1();
+                RunQuestao2();
+                RunQuestao3();
+                RunQuestao4();
+
+            Console.Write(_stringBuilderResult.ToString());
         }
 
         #region Questoes
 
-        private static void PrintQuestao1()
+        private static void RunQuestao1()
         {
             foreach (var areaStats in AreaStatsDict.MaxBy(p => p.Value.MaxSalario))
             {
                 foreach (var funcionario in areaStats.Value.Max)
                 {
-                    Console.WriteLine($"global_max|{GetUTF8String(funcionario.Nome)} {GetUTF8String(funcionario.Sobrenome)}|{areaStats.Value.MaxSalario:0.00}");
+                    _stringBuilderResult.AppendLine($"global_max|{GetUTF8String(funcionario.Nome)} {GetUTF8String(funcionario.Sobrenome)}|{areaStats.Value.MaxSalario:0.00}");
                 }
             }
 
@@ -55,52 +58,52 @@ namespace Desafio5
             {
                 foreach (var funcionario in areaStats.Value.Min)
                 {
-                    Console.WriteLine($"global_min|{GetUTF8String(funcionario.Nome)} {GetUTF8String(funcionario.Sobrenome)}|{areaStats.Value.MinSalario:0.00}");
+                    _stringBuilderResult.AppendLine($"global_min|{GetUTF8String(funcionario.Nome)} {GetUTF8String(funcionario.Sobrenome)}|{areaStats.Value.MinSalario:0.00}");
                 }
             }
 
-            Console.WriteLine($"global_avg|{(_totalSalarios / AreaStatsDict.Sum(a => a.Value.TotalFuncionarios)):0.00}");
+            _stringBuilderResult.AppendLine($"global_avg|{(_totalSalarios / AreaStatsDict.Sum(a => a.Value.TotalFuncionarios)):0.00}");
         }
 
-        private static void PrintQuestao2()
+        private static void RunQuestao2()
         {
             foreach (var areaStats in AreaStatsDict)
             {
                 foreach (var funcionario in areaStats.Value.Max)
                 {
-                    Console.WriteLine($"area_max|{GetUTF8String(AreasDict[areaStats.Key])}|{GetUTF8String(funcionario.Nome)} {GetUTF8String(funcionario.Sobrenome)}|{areaStats.Value.MaxSalario:0.00}");
+                    _stringBuilderResult.AppendLine($"area_max|{GetUTF8String(AreasDict[areaStats.Key])}|{GetUTF8String(funcionario.Nome)} {GetUTF8String(funcionario.Sobrenome)}|{areaStats.Value.MaxSalario:0.00}");
                 }
 
                 foreach (var funcionario in areaStats.Value.Min)
                 {
-                    Console.WriteLine($"area_min|{GetUTF8String(AreasDict[areaStats.Key])}|{GetUTF8String(funcionario.Nome)} {GetUTF8String(funcionario.Sobrenome)}|{areaStats.Value.MinSalario:0.00}");
+                    _stringBuilderResult.AppendLine($"area_min|{GetUTF8String(AreasDict[areaStats.Key])}|{GetUTF8String(funcionario.Nome)} {GetUTF8String(funcionario.Sobrenome)}|{areaStats.Value.MinSalario:0.00}");
                 }
 
-                Console.WriteLine($"area_avg|{GetUTF8String(AreasDict[areaStats.Key])}|{areaStats.Value.Salario / areaStats.Value.TotalFuncionarios:0.00}");
+                _stringBuilderResult.AppendLine($"area_avg|{GetUTF8String(AreasDict[areaStats.Key])}|{areaStats.Value.Salario / areaStats.Value.TotalFuncionarios:0.00}");
             }
         }
 
-        private static void PrintQuestao3()
+        private static void RunQuestao3()
         {
             foreach (var areaStats in AreaStatsDict.MaxBy(p => p.Value.TotalFuncionarios))
             {
-                Console.WriteLine($"most_employees|{GetUTF8String(AreasDict[areaStats.Key])}|{areaStats.Value.TotalFuncionarios}");
+                _stringBuilderResult.AppendLine($"most_employees|{GetUTF8String(AreasDict[areaStats.Key])}|{areaStats.Value.TotalFuncionarios}");
             }
 
             foreach (var areaStats in AreaStatsDict.MinBy(p => p.Value.TotalFuncionarios))
             {
-                Console.WriteLine($"least_employees|{GetUTF8String(AreasDict[areaStats.Key])}|{areaStats.Value.TotalFuncionarios}");
+                _stringBuilderResult.AppendLine($"least_employees|{GetUTF8String(AreasDict[areaStats.Key])}|{areaStats.Value.TotalFuncionarios}");
             }
         }
 
-        private static void PrintQuestao4()
+        private static void RunQuestao4()
         {
             foreach (var sobrenomeStats in SobrenomeStatsDict.Where(p => p.Value.TotalFuncionarios > 1))
             {
                 foreach (var nome in sobrenomeStats.Value.Nomes)
                 {
                     var sobrenome = GetUTF8String(sobrenomeStats.Value.Sobrenome);
-                    Console.WriteLine($"last_name_max|{sobrenome}|{GetUTF8String(nome)} {sobrenome}|{sobrenomeStats.Value.MaxSalario:0.00}");
+                    _stringBuilderResult.AppendLine($"last_name_max|{sobrenome}|{GetUTF8String(nome)} {sobrenome}|{sobrenomeStats.Value.MaxSalario:0.00}");
                 }
             }
         }
@@ -109,7 +112,19 @@ namespace Desafio5
 
         #region Utils
 
+        public static unsafe String ToThinString(byte[] src)
+        {
+            int c;
+            var ret = String.Empty;
+            if ((c = Array.IndexOf(src, byte.MinValue)) > 0)
+                fixed (char* dst = (ret = new String('\0', c)))
+                    do
+                        dst[--c] = (char)src[c];  // fill new String by in-situ mutation
+                    while (c > 0);
 
+            return ret;
+        }
+        
         private static string GetUTF8String(byte[] bytes) => Encoding.UTF8.GetString(bytes).Replace("\0", string.Empty);
 
         public static unsafe string GetUnicodeString(byte[] bytes)
@@ -120,7 +135,7 @@ namespace Desafio5
 
                 for (int i = 0; i < bytes.Length && pB[i] != 0; i++)
                 {
-                    _stringBuilderHashCode.Append(Convert.ToChar(pB[i]));
+                    _stringBuilderHashCode.Append((char)pB[i]);
                 }
 
                 return _stringBuilderHashCode.ToString();
@@ -131,7 +146,6 @@ namespace Desafio5
         {
             double result = 0;
             var len = value.Length;
-            if (len == 0) return Double.NaN;
             double sign = 1;
 
             fixed (char* pstr = value)
@@ -140,27 +154,20 @@ namespace Desafio5
                 var pc = pstr;
                 char c = *pc;
 
-                if (c == '-')
-                {
-                    sign = -1;
-                    ++pc;
-                    if (pc >= end) return Double.NaN;
-                }
-
                 while (true)
                 {
-                    if (pc >= end) return sign * result;
                     c = *pc++;
+                    
                     if (c < '0' || c > '9') break;
+                    
                     result = (result * 10.0) + (c - '0');
                 }
-
-                if (c != '.' && c != ',') return Double.NaN;
+           
                 double exp = 0.1;
+                
                 while (pc < end)
                 {
                     c = *pc++;
-                    if (c < '0' || c > '9') return Double.NaN;
                     result += (c - '0') * exp;
                     exp *= 0.1;
                 }
@@ -199,9 +206,8 @@ namespace Desafio5
                 var maxValueLength = 27;
                 var maxPropLength = 12;
 
-                var funcionariosPropertyName = new byte[maxPropLength];
+                var funcionariosCurrentProperty = 0;
                 var funcionariosPropertyValue = new byte[maxValueLength];
-                var funcionariosPropertyNameIndex = 0;
                 var funcionariosPropertyValueIndex = 0;
 
                 var areasPropertyName = new byte[maxPropLength];
@@ -212,11 +218,7 @@ namespace Desafio5
                 var value = new StringBuilder();
                 var currentArea = new Area();
 
-                const int propNameId = 25705;
                 const int propNameNome = 1701670766;
-                const int propNameSobrenome = 1919053683;
-                const int propNameSalario = 1634492787;
-                const int propNameArea = 1634038369;
                 const int propNameCodigo = 1768189795;
 
                 int read;
@@ -276,22 +278,7 @@ namespace Desafio5
                                     }
                                 case Operations.FuncionariosItemReadPropName:
                                     {
-                                        funcionariosPropertyName = new byte[maxPropLength];
-                                        funcionariosPropertyNameIndex = 0;
-
-                                        while (true)
-                                        {
-                                            if (i == read)
-                                            {
-                                                i = 0;
-                                                read = sr.Read(buffer, 0, buffer.Length);
-                                            }
-
-                                            if (pBuffer[i] != '"')
-                                                funcionariosPropertyName[funcionariosPropertyNameIndex++] = pBuffer[i++];
-                                            else
-                                                break;
-                                        }
+                                        funcionariosCurrentProperty++;
 
                                         MoveToToken(sr, buffer, ref read, pBuffer, ref i, ':');
                                         operation = Operations.FuncionariosItemReadValue;
@@ -302,20 +289,6 @@ namespace Desafio5
                                     {
                                         funcionariosPropertyValue = new byte[maxValueLength];
                                         funcionariosPropertyValueIndex = 0;
-
-                                        while (true)
-                                        {
-                                            if (i == read)
-                                            {
-                                                i = 0;
-                                                read = sr.Read(buffer, 0, buffer.Length);
-                                            }
-
-                                            if (pBuffer[i] == '"' && pBuffer[i] == ' ' && pBuffer[i] == '\n')
-                                                i++;
-                                            else
-                                                break;
-                                        }
 
                                         if (pBuffer[i] == '"') // if string
                                         {
@@ -330,9 +303,7 @@ namespace Desafio5
                                                 }
 
                                                 if (pBuffer[i] == '"')
-                                                {
                                                     break;
-                                                }
                                                 else
                                                     funcionariosPropertyValue[funcionariosPropertyValueIndex++] = pBuffer[i++];
                                             }
@@ -349,35 +320,37 @@ namespace Desafio5
 
                                                 if (pBuffer[i] == ',')
                                                     break;
+                                                else if (funcionariosCurrentProperty != 1)
+                                                    value.Append((char)pBuffer[i++]);
                                                 else
-                                                    value.Append(Convert.ToChar(pBuffer[i++]));
+                                                    i++;
                                             }
                                         }
 
-                                        switch (BitConverter.ToInt32(funcionariosPropertyName, 0))
+                                        switch (funcionariosCurrentProperty)
                                         {
-                                            case propNameId:
+                                            case 1:
 
                                                 operation = Operations.FuncionariosItemMoveToPropStart;
-                                                value.Clear();
+                                                //value.Clear();
 
                                                 break;
 
-                                            case propNameNome:
+                                            case 2:
 
                                                 _currentFuncionario.Nome = funcionariosPropertyValue;
                                                 operation = Operations.FuncionariosItemMoveToPropStart;
 
                                                 break;
 
-                                            case propNameSobrenome:
+                                            case 3:
 
                                                 _currentFuncionario.Sobrenome = funcionariosPropertyValue;
                                                 operation = Operations.FuncionariosItemMoveToPropStart;
 
                                                 break;
 
-                                            case propNameSalario:
+                                            case 4:
 
                                                 _currentFuncionario.Salario = DoubleParse(value.ToString());
 
@@ -386,12 +359,14 @@ namespace Desafio5
 
                                                 break;
 
-                                            case propNameArea:
+                                            case 5:
 
+                                                funcionariosCurrentProperty = 0;
                                                 _currentFuncionario.Area = BitConverter.ToInt32(funcionariosPropertyValue, 0);
                                                 operation = Operations.FuncionariosItemMoveToPropStart;
 
                                                 ComputeSobrenomeStats();
+                                                
                                                 ComputeAreaStats();
 
                                                 break;
@@ -474,20 +449,6 @@ namespace Desafio5
                                     {
                                         areasPropertyValue = new byte[maxValueLength];
                                         areasPropertyValueIndex = 0;
-
-                                        while (true)
-                                        {
-                                            if (i == read)
-                                            {
-                                                i = 0;
-                                                read = sr.Read(buffer, 0, buffer.Length);
-                                            }
-
-                                            if (pBuffer[i] == '"' && pBuffer[i] == ' ' && pBuffer[i] == '\n')
-                                                i++;
-                                            else
-                                                break;
-                                        }
 
                                         i++;
 
